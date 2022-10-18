425406b9f5cde36b48fe6d6077267a272793b66b
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-18 13:28:49 -0500

    added google marker component to custom marker

--- src/components/Marker/CustomMarker.jsx
+++ src/components/Marker/CustomMarker.jsx
@@ -1,8 +1,24 @@
+import { Marker } from "@react-google-maps/api";
+import { useSelector } from "react-redux";
 
 // create a marker component that customizes the fountain markers.
 function CustomMarker() {
+    // import fountain data from redux.
+    const fountains = useSelector(store => store.fountainReducer);
+
+
     return (
-        <div></div>
+        <>
+        {fountains.map(ftn => (
+            <Marker
+                key={ftn.id}
+                position={{lat: Number(ftn.latitude), lng: Number(ftn.longitude)}}
+                
+            >
+
+            </Marker>
+        ))}
+        </>
     );
 }
 
