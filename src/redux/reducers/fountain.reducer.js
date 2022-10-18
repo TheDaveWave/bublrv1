d3257fb270fb1d5c5d34d8e2281410b27106cbec
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-18 11:03:37 -0500

    setup fountain reducer to store fountain data

--- src/redux/reducers/fountain.reducer.js
+++ src/redux/reducers/fountain.reducer.js
@@ -1,5 +1,9 @@
 // a reducer to store the fountains.
 const fountainReducer = (state=[], action) => {
-
-
+    switch (action.type) {
+        case 'SET_FOUNTAINS':
+            return action.payload;
+        default:
+            return state;
+    }
 };
