b0458f2f5141ff252b797b2a725ccf01fa12b9bd
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-18 10:42:07 -0500

    added request to get all fountains and a fountain by id

--- server/routes/fountain.router.js
+++ server/routes/fountain.router.js
@@ -2,11 +2,34 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
+// GET all of the fountain data from the database.
 router.get('/', (req, res) => {
-  // GET route code here
+    // setup SQL query text.
+    const queryText = `SELECT * FROM "fountains";`;
+    pool.query(queryText)
+    .then(response => {
+        res.send(response.rows);
+    })
+    .catch(err => {
+        console.log(`Error in getting fountains`, err);
+        res.sendStatus(500);
+    });
+});
+
+// GET route to get fountain by id.
+router.get('/:ftnId', (req, res) => {
+    // user params to get the value of the id.
+    const ftnId = req.params.ftnId;
+    // SQL query text using data sanitization.
+    const queryText = `SELECT * FROM "fountains" WHERE "id"=$1;`;
+    pool.query(queryText, [ftnId])
+    .then(response => {
+        res.send(response.rows);
+    })
+    .catch(err => {
+        console.log(`Error getting fountain with id:${ftnId}`, err);
+        res.sendStatus(500);
+    });
 });
 
 /**
