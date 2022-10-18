2ac0cc254b087c2e2c664c2a12b62726847a65bf
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-18 18:25:13 -0500

    added display and buttons for each fountainlistitem

--- src/components/FountainsPage/FountainsListItem.jsx
+++ src/components/FountainsPage/FountainsListItem.jsx
@@ -1,7 +1,17 @@
 
 function FountainsListItem({fountain}) {
     return (
-        <div>This is a fountain</div>
+        <div>
+            <img src={fountain.picture} className='info-img' alt='A drinking fountain'/>
+            <div>
+                <p>Rating: {fountain.rating}</p>
+            </div>
+            <div>
+                <button>Go</button>
+                <button>Reviews</button>
+                <button>Rate</button>
+            </div>
+        </div>
     );
 }
 
