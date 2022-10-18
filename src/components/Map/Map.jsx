dc3bff8b44a59c83814dad140783ff5df5debb59
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-18 14:08:48 -0500

    had to move googlemaps to a map component so it could load google.maps properties properly

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,71 @@
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import CustomMarker from "../Marker/CustomMarker";
+import './Map.css';
+
+// component to display the map page.
+function Map() {
+    // setup local state for the map.
+    // lat and lng are set to fargo, ND.
+    const [lng, setLng] = useState(46.8772);
+    const [lat, setLat] = useState(-96.7898);
+
+    
+    // access useDispatch().
+    const dispatch = useDispatch();
+
+    // function to get the current location of the user. 
+    const getLocation = () => {
+        // check if user has location services enabled.
+        if(navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(p => {
+                // set lat local state to current latitude.
+                setLat(p.coords.latitude);
+                // set lng local state to current longitude.
+                setLng(p.coords.longitude);
+                // console log for testing
+                console.log(p.coords.latitude, p.coords.longitude);
+            });
+        }
+        else {
+            alert('Navigation services are not enabled.');
+        }
+    }
+
+    // get fountains on load.
+    useEffect(() => {
+        dispatch({type: 'GET_FOUNTAINS'});
+        getLocation();
+    }, []);
+
+
+    // Google Maps
+
+    // create center object using user's location
+    const center = {lat: lat, lng: lng};
+
+    // create settings for the google marker icon property
+    const customUserIcon = {
+        url: '/svg/user_marker_red.svg',
+        scaledSize: new window.google.maps.Size(40, 40),
+        origin: new window.google.maps.Point(0, 0),
+        anchor: new window.google.maps.Point(20, 20)
+    }
+
+    return (
+        <main>
+            <GoogleMap
+            // setup properties of the map for it to function.
+            zoom={13}
+            center={center}
+            mapContainerClassName='map-container'
+            >
+                <Marker position={center} icon={customUserIcon}></Marker>
+                <CustomMarker />
+            </GoogleMap>
+        </main>
+    );
+}
+
+export default Map;
--- src/components/Maps/MapPage.jsx
+++ src/components/Maps/MapPage.jsx
@@ -1,73 +1,28 @@
-import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import CustomMarker from "../Marker/CustomMarker";
-import './MapPage.css';
+import Map from "../Map/Map";
 
 // component to display the map page.
 function MapPage() {
-    // setup local state for the map.
-    // lat and lng are set to fargo, ND.
-    const [lng, setLng] = useState(46.8772);
-    const [lat, setLat] = useState(-96.7898);
-
-    
     // access useDispatch().
     const dispatch = useDispatch();
-
-    // function to get the current location of the user. 
-    const getLocation = () => {
-        // check if user has location services enabled.
-        if(navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(p => {
-                // set lat local state to current latitude.
-                setLat(p.coords.latitude);
-                // set lng local state to current longitude.
-                setLng(p.coords.longitude);
-                // console log for testing
-                console.log(p.coords.latitude, p.coords.longitude);
-            });
-        }
-        else {
-            alert('Navigation services are not enabled.');
-        }
-    }
 
     // get fountains on load.
     useEffect(() => {
         dispatch({type: 'GET_FOUNTAINS'});
-        getLocation();
     }, []);
 
 
-    // Google Maps
-
-    // create center object using user's location
-    const center = {lat: lat, lng: lng};
-
-    
     // extract isLoaded key from useLoadScript
     const { isLoaded } = useLoadScript({
         // access the google maps API key.
         googleMapsApiKey: 'AIzaSyDpIuGzfvVnYjbuWH99wXaEDuCfFuPjwdM',
     });
 
-
-    // if the map is not yet loaded return a loading div.
-    if(!isLoaded) {
-        return <div>Loading... </div>
-    }
-
     return (
         <main>
-            <GoogleMap
-            // setup properties of the map for it to function.
-            zoom={13}
-            center={center}
-            mapContainerClassName='map-container'
-            >
-                <CustomMarker />
-            </GoogleMap>
+            {isLoaded ? <Map /> : <div>Loading...</div>}
         </main>
     );
 }
