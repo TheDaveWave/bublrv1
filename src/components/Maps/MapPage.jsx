7419173bfb3c3b6055a169018272e103e818ae5e
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-18 11:42:17 -0500

    imported googlemaps api into mappage

--- src/components/Maps/MapPage.jsx
+++ src/components/Maps/MapPage.jsx
@@ -1,9 +1,36 @@
+import { useLoadScript } from "@react-google-maps/api";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 // component to display the map page.
 function MapPage() {
+    // access useDispatch().
+    const dispatch = useDispatch();
+
+
+    
+    // get fountains on load.
+    useEffect(() => {
+        dispatch({type: 'GET_FOUNTAINS'});
+    }, []);
+
+    // Google Maps
+    
+    // extract isLoaded key from useLoadScript
+    const { isLoaded } = useLoadScript({
+        // access the google maps API key.
+        googleMapsApiKey: 'AIzaSyDpIuGzfvVnYjbuWH99wXaEDuCfFuPjwdM',
+    });
+
+
+    // if the map is not yet loaded return a loading div.
+    if(!isLoaded) {
+        return <div>Loading... </div>
+    }
+
     return (
         <main>
-            <h1>A Map</h1>
+            
         </main>
     );
 }
