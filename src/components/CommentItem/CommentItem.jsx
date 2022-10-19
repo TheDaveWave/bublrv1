1d829fc6a861fbfac6ddf78313677a0f95c9f643
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-19 17:54:00 -0500

    added a reply button onto each comment

--- src/components/CommentItem/CommentItem.jsx
+++ src/components/CommentItem/CommentItem.jsx
@@ -1,9 +1,11 @@
-import Replies from "../Replies/Replies";
 
 function CommentItem({comment}) {
+
+
     return (
         <>
         <li>ID: {comment.id} | User: {comment.username} | Comment: {comment.body} | Likes: {comment.likes} | Date: {comment.date}</li>
+        <button>Reply</button>
         </>
     );
 }
