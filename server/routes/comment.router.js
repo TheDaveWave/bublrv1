ba60ad3f61412eaa3a93453ba3ef821ac60ad133
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-19 14:02:44 -0500

    created route to get all the comments for a fountain

--- server/routes/comment.router.js
+++ server/routes/comment.router.js
@@ -2,11 +2,23 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
-router.get('/', (req, res) => {
-  // GET route code here
+// GET route to retrieve all the comments for a specific fountain.
+router.get('/:ftnId', (req, res) => {
+  // get the ftnId from the url params.
+  const ftnId = req.params.ftnId;
+  // setup SQL query text.
+  const queryText = `SELECT "c".*
+  FROM "comments" AS "c"
+  JOIN "fountains" AS "f" ON "f"."id" = "c"."fountain_id"
+  WHERE "f"."id"=$1;`;
+  pool.query(queryText, [ftnId])
+  .then(response => {
+    res.send(response.rows);
+  })
+  .catch(err => {
+    console.log(`Error getting all comments for fountain with id: ${ftnId}`, err);
+    res.sendStatus(500);
+  });
 });
 
 /**
