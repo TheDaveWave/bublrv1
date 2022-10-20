7cfb29c9da18fc86751144be3739fd23a04ba52a
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-20 09:49:52 -0500

    conditionally rendered delete reply button to appear only for the user who made the reply

--- src/components/Replies/RepyItem.jsx
+++ src/components/Replies/RepyItem.jsx
@@ -1,7 +1,22 @@
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
-function ReplyItem() {
+function ReplyItem({reply}) {
+
+    // setup access to useDispatch()
+    const dispatch = useDispatch();
+    // get access to current user id.
+    const userid = useSelector(store => store.user.id);
+
+    const deleteReply = () => {
+
+    }
+
     return (
-        <></>
+        <>
+        <li key={reply?.id}>User: {reply.username} | Reply: {reply.body} | Likes {reply.likes} | Date: {reply.date}</li>
+        {userid === Number(reply.user_id) && <button>Delete</button>}
+        </>
     );
 }
 
