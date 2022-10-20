5abff4625b772cd275b357510a0caa460c72eca4
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-20 10:05:46 -0500

    enabled user to delete their replies

--- src/components/Replies/RepyItem.jsx
+++ src/components/Replies/RepyItem.jsx
@@ -2,20 +2,23 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 
 function ReplyItem({reply}) {
-
     // setup access to useDispatch()
     const dispatch = useDispatch();
     // get access to current user id.
     const userid = useSelector(store => store.user.id);
-
-    const deleteReply = () => {
-
+    // used to delete a reply with a given id.
+    const deleteReply = (id) => {
+        // dispatch to deleteReply saga.
+        dispatch({
+            type: 'DELETE_REPLY',
+            payload: id
+        });
     }
 
     return (
         <>
         <li key={reply?.id}>User: {reply.username} | Reply: {reply.body} | Likes {reply.likes} | Date: {reply.date}</li>
-        {userid === Number(reply.user_id) && <button>Delete</button>}
+        {userid === Number(reply.user_id) && <button onClick={() => deleteReply(reply.id)}>Delete</button>}
         </>
     );
 }
