f9729b16d584fe0c5a3ac66af9398cef8a43d4b7
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-21 11:32:07 -0500

    added edit mode for bio

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  // setup local state.
+  const [editMode, setEditMode] = useState(false);
+  const [bio, setBio] = useState(user.bio || 'Empty');
+
+
   return (
-    <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
-    </div>
+    <main>
+      <div>
+        <h2>{user.username}</h2>
+        {/* <p>Your ID is: {user.id}</p> */}
+        <h3>Bio</h3>
+      </div>
+      <div>
+        <textarea value={bio} onChange={evt => setBio(evt.target.value)} readOnly={editMode ? false : true}></textarea>
+        <div>
+          {editMode ? 
+            <>
+            <button>Save</button>
+            <button onClick={() => setEditMode(false)}>Cancel</button> 
+            </> :
+            <button onClick={() => setEditMode(true)}>Edit</button>
+          }         
+        </div>
+      </div>
+      <LogOutButton />
+    </main>
   );
 }
 
