c18635f959fcc5de709090d23addaa18503ffefb
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-21 10:28:07 -0500

    used material ui to display rating

--- src/components/FountainsPage/FountainsListItem.jsx
+++ src/components/FountainsPage/FountainsListItem.jsx
@@ -1,20 +1,26 @@
+import { Rating } from "@mui/material";
+import { useState } from "react";
 import { useHistory } from "react-router-dom";
 
 function FountainsListItem({fountain}) {
     // access browser history
     const history = useHistory();
+    // setup local state for rating
+    const [rating, setRating] = useState(Number(fountain.rating));
+    console.log(fountain.id, rating);
 
     return (
         <div>
             <img src={fountain.picture} className='info-img' alt='A drinking fountain'/>
             <div>
-                <p>Rating: {fountain.rating}</p>
+                {/* <p>Rating: {fountain.rating}</p> */}
+                <Rating name='read-only' value={Number(fountain.rating)} precision={0.1} readOnly/>
                 <p>Likes: {fountain.likes}</p>
             </div>
             <div>
                 <button>Go</button>
                 <button onClick={() => history.push(`/fountain/${fountain.id}`)}>Comments</button>
-                <button>Rate</button>
+                {/* <button>Rate</button> */}
             </div>
         </div>
     );
