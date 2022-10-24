69a86ec5b9907eab4eb0e71b6a9256610cbce0e9
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-24 15:14:16 -0500

    got flexbox to evenly space header, content, and footer

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -25,6 +25,7 @@ import './App.css';
 import FountainsPage from '../FountainsPage/FountainsPage';
 import FtnDetailView from '../FountainsPage/FtnDetailView';
 import SettingsPage from '../AdminPage/AdminPage';
+import { Box } from '@mui/material';
 
 function App() {
   const dispatch = useDispatch();
@@ -38,118 +39,137 @@ function App() {
   return (
     <Router>
       <div>
-        <Nav />
-        <Switch>
-          {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-          <Redirect exact from="/" to="/home" />
+        <Box 
+          component='div' 
+          sx={{
+            display: 'flex',
+            minHeight: '100vh',
+            flexDirection: 'column',
+            justifyContent: 'space-between'
+          }}
+        >
+          <Nav />
+          <Box className='content' component='div'>
+            <Switch>
+              {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
+              <Redirect exact from="/" to="/home" />
 
-          {/* Visiting localhost:3000/about will show the about page. */}
-          <Route
-            // shows AboutPage at all times (logged in or not)
-            exact
-            path="/about"
+              {/* Visiting localhost:3000/about will show the about page. */}
+              <Route
+                // shows AboutPage at all times (logged in or not)
+                exact
+                path="/about"
+              >
+                <AboutPage />
+              </Route>
+
+              {/* For protected routes, the view could show one of several things on the same route.
+                Visiting localhost:3000/user will show the UserPage if the user is logged in.
+                If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
+                Even though it seems like they are different pages, the user is always on localhost:3000/user */}
+              <ProtectedRoute
+                // logged in shows UserPage else shows LoginPage
+                exact
+                path="/user"
+              >
+                <UserPage />
+              </ProtectedRoute>
+
+              <ProtectedRoute
+                // logged in can visit the maps page.
+                exact path='/maps'
+              >
+                <MapPage />
+              </ProtectedRoute>
+
+              <ProtectedRoute
+                exact path='/fountains'
+              >
+                <FountainsPage />
+              </ProtectedRoute>
+
+              <ProtectedRoute
+                path='/fountain/:ftnId'
+              >
+                <FtnDetailView />
+              </ProtectedRoute>
+
+              <ProtectedRoute
+                // logged in shows InfoPage else shows LoginPage
+                exact
+                path="/info"
+              >
+                <InfoPage />
+              </ProtectedRoute>
+
+              <ProtectedRoute
+                exact path='/admin'
+              >
+                {user.admin ? 
+                  <SettingsPage />
+                  :
+                  <Redirect to="/user" />
+                }
+              </ProtectedRoute>
+
+              <Route
+                exact
+                path="/login"
+              >
+                {user.id ?
+                  // If the user is already logged in, 
+                  // redirect to the /user page
+                  <Redirect to="/user" />
+                  :
+                  // Otherwise, show the login page
+                  <LoginPage />
+                }
+              </Route>
+
+              <Route
+                exact
+                path="/registration"
+              >
+                {user.id ?
+                  // If the user is already logged in, 
+                  // redirect them to the /user page
+                  <Redirect to="/user" />
+                  :
+                  // Otherwise, show the registration page
+                  <RegisterPage />
+                }
+              </Route>
+
+              <Route
+                exact
+                path="/home"
+              >
+                {/* {user.id ?
+                  // If the user is already logged in, 
+                  // redirect them to the /user page
+                  <Redirect to="/user" />
+                  :
+                  // Otherwise, show the Landing page
+                  <LandingPage />
+                } */}
+                <LandingPage />
+              </Route>
+
+              {/* If none of the other routes matched, we will show a 404. */}
+              <Route>
+                <h1>404</h1>
+              </Route>
+            </Switch>
+          </Box>
+          <Box
+            sx={{
+              position: 'sticky',
+              bottom: '0',
+            }}
           >
-            <AboutPage />
-          </Route>
-
-          {/* For protected routes, the view could show one of several things on the same route.
-            Visiting localhost:3000/user will show the UserPage if the user is logged in.
-            If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
-            Even though it seems like they are different pages, the user is always on localhost:3000/user */}
-          <ProtectedRoute
-            // logged in shows UserPage else shows LoginPage
-            exact
-            path="/user"
-          >
-            <UserPage />
-          </ProtectedRoute>
-
-          <ProtectedRoute
-            // logged in can visit the maps page.
-            exact path='/maps'
-          >
-            <MapPage />
-          </ProtectedRoute>
-
-          <ProtectedRoute
-            exact path='/fountains'
-          >
-            <FountainsPage />
-          </ProtectedRoute>
-
-          <ProtectedRoute
-            path='/fountain/:ftnId'
-          >
-            <FtnDetailView />
-          </ProtectedRoute>
-
-          <ProtectedRoute
-            // logged in shows InfoPage else shows LoginPage
-            exact
-            path="/info"
-          >
-            <InfoPage />
-          </ProtectedRoute>
-
-          <ProtectedRoute
-            exact path='/admin'
-          >
-            {user.admin ? 
-              <SettingsPage />
-              :
-              <Redirect to="/user" />
-            }
-          </ProtectedRoute>
-
-          <Route
-            exact
-            path="/login"
-          >
-            {user.id ?
-              // If the user is already logged in, 
-              // redirect to the /user page
-              <Redirect to="/user" />
-              :
-              // Otherwise, show the login page
-              <LoginPage />
-            }
-          </Route>
-
-          <Route
-            exact
-            path="/registration"
-          >
-            {user.id ?
-              // If the user is already logged in, 
-              // redirect them to the /user page
-              <Redirect to="/user" />
-              :
-              // Otherwise, show the registration page
-              <RegisterPage />
-            }
-          </Route>
-
-          <Route
-            exact
-            path="/home"
-          >
-            {/* {user.id ?
-              // If the user is already logged in, 
-              // redirect them to the /user page
-              <Redirect to="/user" />
-              :
-              // Otherwise, show the Landing page
-              <LandingPage />
-            } */}
-            <LandingPage />
-          </Route>
-
-          {/* If none of the other routes matched, we will show a 404. */}
-          <Route>
-            <h1>404</h1>
-          </Route>
-        </Switch>
-        <Footer />
+            <Footer />
+          </Box>
+        </Box>
       </div>
     </Router>
   );
