ebffc945647c4a489de25ed531b6137ed9e4ab10
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-25 12:39:00 -0500

    added component that will be used to display the current page on the app bar

---
+++ src/components/Nav/PageDisplay.jsx
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+
+function PageDisplay() {
+    return (
+        <Box>
+            <Typography>
+                Hi
+            </Typography>
+        </Box>
+    );
+}
+
+export default PageDisplay;
