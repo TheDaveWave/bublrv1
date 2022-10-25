2e1639d0f12668832fe3c24a9a7e8ed76d69244e
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-25 12:55:11 -0500

    added the path reducer to the root reducer

---
+++ src/redux/reducers/path.reducer.js
@@ -0,0 +1,10 @@
+const pathReducer = (state = '', action) => {
+    switch (action.type) {
+        case 'SET_PAGE':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+export default pathReducer;
