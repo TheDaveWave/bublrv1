a019d45fe6d0478bf1dc3b50bf4b0fd5902374af
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-28 15:21:07 -0500

    added to the pool.js

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -28,9 +28,11 @@ if (process.env.DATABASE_URL) {
   };
 } else {
   config = {
-    host: 'localhost', // Server hosting the postgres database
-    port: 5432, // env var: PGPORT
-    database: 'wasserberg', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
+    user: process.env.PG_USER || null,
+    password: process.env.DATABASE_SECRET || null,
+    host: process.env.DATABASE_SERVER || 'localhost', // Server hosting the postgres database
+    port: process.env.DATABASE_PORT || 5432, // env var: PGPORT
+    database: process.env.DATABASE_NAME || 'wasserberg', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
